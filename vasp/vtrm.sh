ad40dc6f7126cf4008d3feaeea842a5f305a5dd2
hsulab <ahcigar@foxmail.com>
2020-07-27 16:32:59 +0100

    add scripts which remove extra output files

---
+++ vasp/vtrm.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+# ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+# Author: Jiayan Xu
+# Description:
+#   Remove redundant output files.
+# ===== ===== ===== ===== ===== ===== ===== ===== ===== =====
+
+# default saved files
+saved_fnames=('INCAR' 'POSCAR' 'KPOINTS' 'POTCAR' 'CONTCAR' \
+              'WAVECAR' 'CHG' 'CHGCAR' 'ICONST' 'PENALTYPOT' \
+              'vdw_kernel.bindat' \
+              'POSCAR_origin' 'RCOCAR' 'PCVCAR' \
+              'vasp.sh' 'vasp.script' 'fort.188')
+
+# add saved files from arguments
+saved_fnames=(${saved_fnames[@]} $@)
+
+# out some infomation
+echo "Remove redundant files, leaving directories unaffected."
+echo ${saved_fnames[@]}
+
+# remove files
+for file in `ls`
+do 
+  # check if file in saved list
+  removed_flag=0
+  for fname in ${saved_fnames[@]}
+  do 
+    if [ $file == $fname ]
+    then 
+      removed_flag=0
+      break
+    else
+      if [ ! -d $file ]
+      then
+         removed_flag=1
+      fi
+    fi
+  done
+  # remove marked files
+  if [ $removed_flag == 1 ]
+  then
+    echo "remove $file"
+    rm $file
+  fi
+done
