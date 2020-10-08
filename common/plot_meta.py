250fb0f414e14e23af6ecbc4f07cd491f73b58a8
hsulab <ahcigar@foxmail.com>
2020-10-08 21:32:04 +0100

    add 2D support

--- common/plot_meta.py
+++ common/plot_meta.py
@@ -9,19 +9,82 @@ import matplotlib
 matplotlib.use('Agg') #silent mode
 import matplotlib.pyplot as plt
 
+
 def read_fesdat(fesDat):
+    #with open(fesDat, 'r') as reader:
+    #    lines = reader.readlines()
 
-    with open(fesDat, 'r') as reader:
-        lines = reader.readlines()
+    #data = np.array(
+    #    [
+    #        line.strip().split() 
+    #        for line in lines if not line.startswith('#!')
+    #    ], dtype=float
+    #)
 
-    data = np.array(
-        [
-            line.strip().split() 
-            for line in lines if not line.startswith('#!')
-        ], dtype=float
-    )
+    fopen = open(fesDat, 'r')
 
-    return data
+    fields = fopen.readline().strip().split()
+    ncolvar = int((len(fields) - 2) / 2.)
+
+    cvDict = {}
+    for idx in range(ncolvar):
+        cvDict[idx] = {}
+        dataLines = []
+        for jdx in range(4):
+            dataLines.append(fopen.readline())
+        
+        cvDict[idx]['min'] = float(dataLines[0].strip().split()[-1])
+        cvDict[idx]['max'] = float(dataLines[1].strip().split()[-1])
+        cvDict[idx]['nbins'] = int(dataLines[2].strip().split()[-1])
+        cvDict[idx]['periodic'] = dataLines[3].strip().split()[-1]
+
+        print(cvDict[idx])
+
+    dataLinePoint = fopen.tell()
+
+    #print(fopen.readline())
+
+    # get grids
+    lastNumBins = 0
+    for idx in range(ncolvar):
+        fopen.seek(dataLinePoint)
+        dataLists = []
+        for jdx in range(cvDict[idx]['nbins']):
+            dataLists.append(fopen.readline().strip().split())
+            for kdx in range(lastNumBins):
+                line = fopen.readline()
+        dataList = [data[idx] for data in dataLists]
+        cvDict[idx]['points'] = np.array(dataList, dtype=float)
+        if lastNumBins == 0:
+            lastNumBins = cvDict[idx]['nbins']
+        else:
+            lastNumBins = lastNumBins*cvDict[idx]['nbins']
+
+    # free energy
+    # TODOL: only 2 CV for now, maybe extended later
+    freeEnergies = []
+    if ncolvar == 1:
+        fopen.seek(dataLinePoint)
+        curList = []
+        for idx in range(cvDict[0]['nbins']):
+            curList.append(float(fopen.readline().strip().split()[1]))
+        freeEnergies.extend(curList)
+    elif ncolvar == 2:
+        fopen.seek(dataLinePoint)
+        for idx in range(cvDict[1]['nbins']):
+            curList = []
+            for jdx in range(cvDict[0]['nbins']):
+                curList.append(float(fopen.readline().strip().split()[2]))
+            freeEnergies.append(curList)
+            emptyLine = fopen.readline()
+    else:
+        raise ValueError('cannot support number of CV larger than 2')
+
+    freeEnergies = np.array(freeEnergies)
+
+    fopen.close()
+
+    return cvDict, freeEnergies
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser()
@@ -36,19 +99,30 @@ if __name__ == '__main__':
     else:
         for idx in range(args.numfiles):
             datList.append('fes_'+str(idx)+'.dat')
-    print(datList)
 
-    fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12,8))
-    ax.set_title('MetaDynamics Convergence Analysis', \
-            fontsize=20, fontweight='bold')
+    cvDict, freeEnergies = read_fesdat(datList[0])
+    
+    if len(cvDict.keys()) == 1:
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12,8))
+        ax.set_title('MetaDynamics Convergence Analysis', \
+                fontsize=20, fontweight='bold')
 
-    ax.set_xlabel('Collective Variable', fontsize=16)
-    ax.set_ylabel('Free Energy', fontsize=16)
+        ax.set_xlabel('Collective Variable', fontsize=16)
+        ax.set_ylabel('Free Energy', fontsize=16)
 
-    for datFile in datList:
-        data = read_fesdat(datFile)
-        ax.plot(data[:,0]*10, data[:,1], label=datFile)
-
-    ax.legend()
+        for datFile in datList:
+            cvDict, freeEnergies = read_fesdat(datFile)
+            ax.plot(cvDict[0]['points']*10, freeEnergies, label=datFile)
+        ax.legend()
+    elif len(cvDict.keys()) == 2:
+        fig, ax = plt.subplots(nrows=1, ncols=1, figsize=(12,8))
+        cn = ax.contour(cvDict[0]['points'], cvDict[1]['points'], freeEnergies) 
+            #levels=14, linewidth=0.5, color='k')
+        cntr = ax.contourf(cvDict[0]['points'], cvDict[1]['points'], freeEnergies, 
+            levels=12, cmap='RdBu')
+        fig.colorbar(cntr, ax=ax)
+        plt.clabel(cn, inline=True, fontsize=12)
+    else:
+        pass
 
     plt.savefig('fes.png')
