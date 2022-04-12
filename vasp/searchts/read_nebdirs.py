3200122cc8bde4ecc3d707c802dcb76cdbb9af68
hsulab <ahcigar@foxmail.com>
2022-04-12 15:31:02 +0100

    update nebutils

--- vasp/searchts/read_nebdirs.py
+++ vasp/searchts/read_nebdirs.py
@@ -1,39 +1,48 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import argparse
 from pathlib import Path
 
 import numpy as np
 
 from ase.io import read, write
 
+parser = argparse.ArgumentParser()
+parser.add_argument(
+    "FILE"
+)
+args = parser.parse_args()
+
 working_directory = Path('./')
+
+vasp_file = args.FILE
 
 nebdirs = []
 for nebdir in working_directory.glob('[0-9][0-9]'):
     if nebdir.is_dir():
         nebdirs.append(nebdir)
 nebdirs.sort()
+print(nebdirs)
 
 frames = []
 for nebdir in nebdirs:
-    contcar = nebdir / 'CONTCAR'
-    #if contcar.exists():
-    #    print(contcar)
-    #    atoms = read(contcar)
-    #    frames.append(atoms)
-    #else:
-    #    raise ValueError('contcar not found.')
-    outcar = nebdir / 'OUTCAR'
-    if outcar.exists():
-        print(outcar)
-        atoms = read(outcar, '-1')
-        print(atoms.get_potential_energy())
-        print(np.max(np.fabs(atoms.get_forces())))
-        frames.append(atoms)
+    outcar = nebdir / vasp_file
+    if outcar.name in ["OUTCAR", "vasprun.xml"]:
+        if outcar.exists():
+            print(outcar)
+            atoms = read(outcar, '-1')
+            print("energy: ", atoms.get_potential_energy())
+            print("max force: ", np.max(np.fabs(atoms.get_forces())))
+        else:
+            raise ValueError("outcar not found.")
+    else:
+        atoms = read(outcar)
+    frames.append(atoms)
 
 assert len(frames) == len(nebdirs)
-write('neb.xyz', frames)
+#write("neb-"+vasp_file+".xyz", frames)
+write("neb.xyz", frames)
 
 
 if __name__ == '__main__':
